ad581c89849302e9395e26ca0d220c31b05fb9ad
Richie Wales <richiew@utexas.edu>
2021-02-06 03:34:04 -0800

    Initial commit with sample code to query Alpaca API

---
+++ TraderJoe/TraderJoe/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  TraderJoe
+//
+//  Created by Richie Wales on 2/6/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
